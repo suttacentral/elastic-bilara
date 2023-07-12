19379efd1f21bee14351c59b6b3b501e982e5ba9
Mateusz Krzeminski <mateusz.krzeminski@stxnext.com>
2023-07-12 06:39:40 +0200

    Add endpoint that lists root paths for muid

---
+++ backend/app/services/projects/utils.py
@@ -0,0 +1,10 @@
+import re
+
+
+def sort_paths(paths):
+    def extract_key(s):
+        head = s.rsplit("/", 1)[-1]
+        head_parts = re.split(r"(\d+)", head)
+        return [int(part) if part.isdigit() else part for part in head_parts]
+
+    return sorted(paths, key=extract_key)
