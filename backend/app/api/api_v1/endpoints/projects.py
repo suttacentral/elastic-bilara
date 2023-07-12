4526d62b6d37cd8575742ccb1956a3dc8bf815f3
Mateusz Krzeminski <mateusz.krzeminski@stxnext.com>
2023-07-12 08:56:29 +0200

    Show json data (#13)
    * Add get_json_data_for_prefix_in_project endpoint
    
    * Test endpoint

--- backend/app/api/api_v1/endpoints/projects.py
+++ backend/app/api/api_v1/endpoints/projects.py
@@ -1,11 +1,14 @@
+from pathlib import Path
 from typing import Annotated
 
 from app.services.auth import utils
-from app.services.projects.models import ProjectsOut, RootPathsOut
+from app.services.projects.models import JSONDataOut, ProjectsOut, RootPathsOut
 from app.services.projects.utils import sort_paths
+from app.services.users.permissions import can_edit_translation
 from app.services.users.schema import UserData
 from fastapi import APIRouter, Depends, HTTPException, status
 from search.search import Search
+from search.utils import get_json_data
 
 router = APIRouter(prefix="/projects")
 
@@ -24,7 +27,22 @@ async def get_projects(
 async def get_root_paths_for_project(
     user: Annotated[UserData, Depends(utils.get_current_user)], muid: str
 ) -> RootPathsOut:
-    data = sort_paths(search.get_file_paths(muid=muid))
+    data: list[str] = sort_paths(search.get_file_paths(muid=muid))
     if not data:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Project '{muid}' not found")
     return RootPathsOut(root_paths=data)
+
+
+@router.get("/{muid}/{prefix}/", response_model=JSONDataOut)
+async def get_json_data_for_prefix_in_project(
+    user: Annotated[UserData, Depends(utils.get_current_user)], muid: str, prefix: str
+) -> JSONDataOut:
+    file: set[str] = search.get_file_paths(muid=muid, prefix=prefix, _type="file_path")
+    if not file:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Data for project '{muid}' and prefix '{prefix}' not found",
+        )
+    can_edit: bool = can_edit_translation(int(user.github_id), muid)
+    data: dict[str, str] = get_json_data(Path(file.pop()))
+    return JSONDataOut(can_edit=can_edit, data=data)
--- backend/app/tests/api/api_v1/test_projects.py
+++ backend/app/tests/api/api_v1/test_projects.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
@@ -64,7 +65,7 @@ class TestProjects:
     @pytest.mark.asyncio
     @patch("app.api.api_v1.endpoints.projects.search.get_file_paths")
     async def test_get_root_paths_for_project(self, mock_get_file_paths, async_client, mock_get_current_user) -> None:
-        mock_get_file_paths.return_value = ["root/path1", "root/path2"]
+        mock_get_file_paths.return_value = {"root/path1", "root/path2"}
         response = await async_client.get("/projects/translation-en-test/")
         assert response.status_code == 200
         assert "root_paths" in response.json()
@@ -77,8 +78,61 @@ class TestProjects:
     async def test_get_root_paths_for_project_muid_does_not_exist(
         self, mock_get_file_paths, async_client, mock_get_current_user
     ) -> None:
-        mock_get_file_paths.return_value = []
+        mock_get_file_paths.return_value = set()
         response = await async_client.get("/projects/nonexistent_muid/")
         assert response.status_code == 404
         assert "detail" in response.json()
         assert response.json() == {"detail": "Project 'nonexistent_muid' not found"}
+
+    @pytest.mark.asyncio
+    async def test_get_json_data_for_prefix_in_project_unauthenticated(self, async_client) -> None:
+        response = await async_client.get("/projects/translation-en-test/an1.1-10/")
+        assert response.status_code == 401
+        assert "detail" in response.json()
+        assert response.json() == {"detail": "Not authenticated"}
+
+    @pytest.mark.asyncio
+    @patch("app.api.api_v1.endpoints.projects.search.get_file_paths")
+    async def test_get_json_data_for_prefix_in_project_invalid_prefix(
+        self, mock_get_file_paths, async_client, mock_get_current_user
+    ) -> None:
+        mock_get_file_paths.return_value = set()
+        response = await async_client.get("/projects/translation-en-test/nonexistent_prefix/")
+        assert response.status_code == 404
+        assert "detail" in response.json()
+        assert response.json() == {
+            "detail": "Data for project 'translation-en-test' and prefix 'nonexistent_prefix' not found"
+        }
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "can_edit, data",
+        [
+            (True, {"an1.1:0.1": "Test", "an1.1:0.2": "Test2"}),
+            (False, {"an1.1:0.1": "Test", "an1.1:0.2": "Test2"}),
+        ],
+    )
+    @patch("app.api.api_v1.endpoints.projects.search.get_file_paths")
+    @patch("app.api.api_v1.endpoints.projects.can_edit_translation")
+    @patch("app.api.api_v1.endpoints.projects.get_json_data")
+    async def test_get_json_data_for_prefix_in_project(
+        self,
+        mock_get_json_data,
+        mock_can_edit_translation,
+        mock_get_file_paths,
+        async_client,
+        mock_get_current_user,
+        can_edit,
+        data,
+    ) -> None:
+        mock_get_file_paths.return_value = set("root/an1.1-10")
+        mock_can_edit_translation.return_value = can_edit
+        mock_get_json_data.return_value = data
+        response = await async_client.get("/projects/translation-en-test/an1.1-10/")
+        assert response.status_code == 200
+        assert "data" in response.json()
+        assert "can_edit" in response.json()
+        assert response.json() == {
+            "can_edit": can_edit,
+            "data": data,
+        }
