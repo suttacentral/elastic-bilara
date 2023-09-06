8ff0bb04d51d3b46119d9d942786532da0ddff8a
Mateusz Krzeminski <mateusz.krzeminski@stxnext.com>
2023-09-06 10:40:27 +0200

    Create GitManager

--- backend/app/services/git/manager.py
+++ backend/app/services/git/manager.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Literal
 
 import app.services.git.utils as utils
 from app.core.config import settings
@@ -6,11 +7,24 @@ from app.services.users.schema import UserData
 from github import Github
 from github.PaginatedList import PaginatedList
 from github.PullRequest import PullRequest
-from pygit2 import Blob, Commit, Oid, RemoteCallbacks, Repository, Signature, UserPass
+from pygit2 import (
+    GIT_CHECKOUT_FORCE,
+    GIT_STATUS_INDEX_MODIFIED,
+    GIT_STATUS_INDEX_NEW,
+    GIT_STATUS_WT_MODIFIED,
+    GIT_STATUS_WT_NEW,
+    Commit,
+    Oid,
+    RemoteCallbacks,
+    Repository,
+    Signature,
+    UserPass,
+)
 
 
 class GitManager:
     _protected_branches = ("published", "unpublished")
+    _git_status = (GIT_STATUS_INDEX_NEW, GIT_STATUS_INDEX_MODIFIED, GIT_STATUS_WT_MODIFIED, GIT_STATUS_WT_NEW)
 
     def __init__(self, published: Path, unpublished: Path, user: UserData) -> None:
         self.user = user
@@ -20,28 +34,35 @@ class GitManager:
         self.committer: Signature = Signature(name=settings.GITHUB_USERNAME, email=settings.GITHUB_EMAIL)
         self.github: Github = Github(settings.GITHUB_TOKEN)
 
-    def checkout(self, name: str = "published") -> None:
-        self.published.remotes["origin"].fetch(refspecs=[name])
+    def checkout(self, name: str = "published", force: bool = False) -> None:
+        self.published.remotes["origin"].fetch(prune=True)
         if not self.published.branches.get(name):
             self.create_local_branch(name)
-        self.published.checkout(self.published.lookup_reference(f"refs/heads/{name}"))
+        ref = self.published.lookup_reference(f"refs/heads/{name}")
+        if force:
+            self.published.checkout_tree(ref.peel(Commit), strategy=GIT_CHECKOUT_FORCE)
+            self.published.head.set_target(ref.target)
+        self.published.checkout(ref)
 
     def copy_files(self, file_paths: list[Path] | None = None) -> list[Path]:
-        # TODO: changed_files is not working properly with pull requests, still empty commits
         paths: list[Path] = file_paths or []
         changed_files = [path for path in paths if self.has_changes("unpublished", path)]
         for path in changed_files:
-            file_content: Blob | None = GitManager.read_file(self.unpublished, path)
+            file_content: bytes | None = GitManager.read_file(self.unpublished, path)
             GitManager.write_file(Path(self.published.workdir) / path, file_content)
-        GitManager.add(self.published, changed_files)
+        if not GitManager.add(self.published, changed_files):
+            return []
         return changed_files
 
-    def create_local_branch(self, name: str) -> None:
+    def create_local_branch(self, name: str) -> bool:
         remote_branch_ref = f"refs/remotes/origin/{name}"
+        if f"refs/heads/{name}" in self.published.references:
+            return False
         if remote_branch_ref in self.published.references:
             self.published.create_branch(name, self.published.lookup_reference(remote_branch_ref).peel(Commit))
         else:
             self.published.create_branch(name, self.published.revparse_single("HEAD").peel(Commit))
+        return True
 
     def delete_local_branch(self, name: str) -> None:
         self.published.branches.delete(name) if name in self.published.branches else None
@@ -101,13 +122,14 @@ class GitManager:
             self.open_pr(title=pr_title, body=pr_body, head=f"{self.author.name}:{branch}")
 
     def has_changes(self, branch_name: str, path: Path) -> bool:
-        unpublished_commit = GitManager.get_latest_commit(self.unpublished, path)
-        published_commit = GitManager.get_latest_commit(self.published, path, branch_name)
+        unpublished_commit: bytes = GitManager.get_latest_commit(self.unpublished, path)
+        published_commit: bytes = GitManager.get_latest_commit(self.published, path, branch_name)
         return unpublished_commit != published_commit
 
     def process_files(self, branch, message, pr_title, pr_body, file_paths: list[Path] | None = None) -> str:
-        # TODO: empty commits in PRs
         paths: list[Path] = file_paths or []
+        if not paths:
+            return ""
         if len(paths) == 1:
             self.handle_single_file(paths[0], branch, message, pr_title, pr_body)
         else:
@@ -116,48 +138,59 @@ class GitManager:
         self._cleanup(branch)
         return url
 
-    def _cleanup(self, branch: str) -> None:
-        self.checkout()
+    def _cleanup(self, branch: str) -> bool:
+        self.checkout(force=True)
         if not self._is_branch_protected(branch):
             self.delete_local_branch(branch)
+            return True
+        return False
 
     def _process_branch_changes(self, branch, paths: list[Path], message: str) -> None:
         if not paths:
             return
         self.checkout(branch)
         changed_files = self.copy_files(paths)
-        if changed_files:
-            GitManager.commit(self.published, self.author, self.committer, message)
+        if changed_files and GitManager.commit(self.published, self.author, self.committer, message, changed_files):
             GitManager.push(self.published, branch=branch)
 
     def _is_branch_protected(self, name: str) -> bool:
         return name in self._protected_branches
 
     @staticmethod
-    def add(repo: Repository, file_paths: list[Path] | None = None) -> None:
+    def add(repo: Repository, file_paths: list[Path] | None = None) -> bool:
         paths: list[Path] = file_paths or []
-        repo.index.add_all(paths)
+        if not paths:
+            return False
+        for path in paths:
+            repo.index.add(path)
         repo.index.write()
+        return True
 
     @staticmethod
-    def read_file(repo: Repository, file_path: Path, branch="unpublished") -> Blob | None:
+    def read_file(repo: Repository, file_path: Path, branch="unpublished") -> bytes | None:
         branch = repo.branches.get(branch)
         if not branch:
             return None
-        blob = branch.peel().tree[file_path].data
-        return blob
+        try:
+            data = branch.peel().tree[file_path].data
+        except KeyError:
+            return None
+        return data
 
     @staticmethod
-    def write_file(dest_path: Path, data: Blob) -> None:
+    def write_file(dest_path: Path, data: bytes) -> None:
         if not data:
             return None
         with open(dest_path, "wb") as f:
             f.write(data)
 
     @staticmethod
-    def commit(repo: Repository, author: Signature, committer: Signature, message: str) -> None:
-        tree: Oid = repo.index.write_tree()
-        repo.create_commit("HEAD", author, committer, message, tree, [repo.head.peel().hex])
+    def commit(
+        repo: Repository, author: Signature, committer: Signature, message: str, paths: list[Path] = None
+    ) -> Oid | Literal[False]:
+        if not GitManager.has_status_changed(repo, paths):
+            return False
+        return repo.create_commit("HEAD", author, committer, message, repo.index.write_tree(), [repo.head.target])
 
     @staticmethod
     def push(repo, remote_name="origin", branch="unpublished"):
@@ -172,16 +205,30 @@ class GitManager:
         pr.edit(state="closed")
 
     @staticmethod
-    def get_latest_commit(repo: Repository, path: Path, branch_name: str = "unpublished") -> Blob | None:
+    def get_latest_commit(repo: Repository, path: Path, branch_name: str = "unpublished") -> bytes | None:
         branch = repo.branches.get(branch_name)
         if not branch:
             return None
         last_commit = branch.peel()
-        blob = last_commit.tree[path].data
-        return blob
+        try:
+            data = last_commit.tree[path].data
+        except KeyError:
+            return None
+        return data
 
     @staticmethod
     def is_file_in_pr(pr: PullRequest, file_path: Path) -> bool:
         if pr.state == "closed":
             return False
         return any(file_path == Path(file.filename) for file in pr.get_files())
+
+    @staticmethod
+    def has_status_changed(repo: Repository, paths: list[Path] = None) -> bool:
+        if paths:
+            for path in paths:
+                try:
+                    if repo.status_file(str(path)) in GitManager._git_status:
+                        return True
+                except KeyError:
+                    return False
+        return False
