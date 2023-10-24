063609b397661be19b03498309a2ee75b6337049
Mateusz Krzeminski <mateusz.krzeminski@stxnext.com>
2023-10-24 08:16:46 +0200

    Add second worker

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -6,14 +6,6 @@ from app.services.git.manager import GitManager
 from celery import Task
 from github import GithubException
 from pygit2 import GitError
-from redis import Redis
-from redis.lock import Lock
-
-lock = Lock(
-    Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, password=settings.REDIS_PASSWORD),
-    "lock",
-    blocking_timeout=300,
-)
 
 
 class GitTask(Task):
@@ -42,35 +34,33 @@ class PrTask(GitTask):
         super().on_failure(exc, task_id, args, kwargs, einfo)
 
 
-@app.task(name="commit", base=GitTask)
+@app.task(name="commit", base=GitTask, queue="commit_queue")
 def commit(user: dict, file_path: str) -> bool:
-    with lock:
-        path = utils.clean_path(file_path)
-        if not path:
-            return False
-        user_data = UserBase(**user)
-        manager = GitManager(settings.PUBLISHED_DIR, settings.WORK_DIR, user_data)
-        message = f"Translations by {user_data.username} to {path}"
-        if GitManager.add(manager.unpublished, [path]) and GitManager.commit(
-            manager.unpublished, manager.author, manager.committer, message, [path]
-        ):
-            GitManager.push(manager.unpublished, "origin", "unpublished")
-            return True
+    path = utils.clean_path(file_path)
+    if not path:
         return False
+    user_data = UserBase(**user)
+    manager = GitManager(settings.PUBLISHED_DIR, settings.WORK_DIR, user_data)
+    message = f"Translations by {user_data.username} to {path}"
+    if GitManager.add(manager.unpublished, [path]) and GitManager.commit(
+        manager.unpublished, manager.author, manager.committer, message, [path]
+    ):
+        GitManager.push(manager.unpublished, "origin", "unpublished")
+        return True
+    return False
 
 
-@app.task(name="pr", base=PrTask)
+@app.task(name="pr", base=PrTask, queue="pr_queue")
 def pr(user, file_paths) -> str:
-    with lock:
-        user_data = UserBase(**user)
-        paths = [utils.clean_path(path) for path in file_paths]
-        if not paths:
-            return ""
-        manager = GitManager(settings.PUBLISHED_DIR, settings.WORK_DIR, user_data)
-        branch = utils.get_branch_name(manager, paths)
-        commit_message = utils.get_pr_commit_message(branch)
-        pr_title = utils.get_pr_title(branch)
-        pr_body = utils.get_pr_body(user_data)
-        return manager.process_files(
-            message=commit_message, branch=branch, pr_title=pr_title, pr_body=pr_body, file_paths=paths
-        )
+    user_data = UserBase(**user)
+    paths = [utils.clean_path(path) for path in file_paths]
+    if not paths:
+        return ""
+    manager = GitManager(settings.PUBLISHED_DIR, settings.WORK_DIR, user_data)
+    branch = utils.get_branch_name(manager, paths)
+    commit_message = utils.get_pr_commit_message(branch)
+    pr_title = utils.get_pr_title(branch)
+    pr_body = utils.get_pr_body(user_data)
+    return manager.process_files(
+        message=commit_message, branch=branch, pr_title=pr_title, pr_body=pr_body, file_paths=paths
+    )
