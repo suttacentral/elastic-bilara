62e5895e453c15c2b89dc72d75f6c1246b70ed9e
Mateusz Krzeminski <mateusz.krzeminski@stxnext.com>
2024-01-23 09:11:33 +0100

    Test delete path endpoint

--- backend/app/tests/api/api_v1/test_directories.py
+++ backend/app/tests/api/api_v1/test_directories.py
@@ -53,3 +53,80 @@ class TestDirectories:
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         assert "detail" in response.json()
         assert response.json()["detail"] == "Could not validate credentials"
+
+    @pytest.mark.asyncio
+    async def test_delete_path_unauthorized(self, async_client):
+        response = await async_client.delete(
+            "/directories/translation/en/test/sutta/test/test1/", params={"dry_run": True}
+        )
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert "detail" in response.json()
+        assert response.json()["detail"] == "Could not validate credentials"
+
+    @pytest.mark.asyncio
+    @patch("app.api.api_v1.endpoints.directories.can_delete_projects")
+    async def test_delete_path_user_writer(
+        self,
+        mock_can_delete_projects,
+        async_client,
+        mock_validate_path,
+        mock_get_current_user,
+    ):
+        mock_can_delete_projects.return_value = False
+        response = await async_client.delete(
+            "/directories/translation/en/test/sutta/test/test1/", params={"dry_run": True}
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert "detail" in response.json()
+        assert response.json()["detail"] == "You are not allowed to delete projects"
+
+    @pytest.mark.asyncio
+    @patch("app.api.api_v1.endpoints.directories.Remover")
+    @patch("app.api.api_v1.endpoints.directories.can_delete_projects")
+    async def test_delete_path_dry_run(
+        self,
+        mock_can_delete_projects,
+        mock_remover,
+        async_client,
+        mock_validate_path,
+        mock_get_current_user_admin,
+    ):
+        remover_instance = mock_remover.return_value
+        root_path_str = "/root/pli/ms/sutta/test/test1/"
+        translation_path_str = "/translation/en/test/sutta/test/test1/"
+        remover_instance.delete_dry.return_value = [root_path_str, translation_path_str]
+        mock_can_delete_projects.return_value = True
+        response = await async_client.delete(f"/directories/{translation_path_str}", params={"dry_run": True})
+        assert response.status_code == status.HTTP_200_OK
+        assert "message" in response.json()
+        assert "results" in response.json()
+        assert response.json()["message"] == "Dry run successful"
+        assert root_path_str in response.json()["results"][0]["path"]
+        assert translation_path_str in response.json()["results"][1]["path"]
+
+    @pytest.mark.asyncio
+    @patch("app.api.api_v1.endpoints.directories.Remover")
+    @patch("app.api.api_v1.endpoints.directories.can_delete_projects")
+    async def test_delete_path(
+        self,
+        mock_can_delete_projects,
+        mock_remover,
+        async_client,
+        mock_validate_path,
+        mock_get_current_user_admin,
+    ):
+        remover_instance = mock_remover.return_value
+        main_path_task_id = "main"
+        related_paths_task_id = "related"
+        remover_instance.delete.return_value = (main_path_task_id, related_paths_task_id)
+        mock_can_delete_projects.return_value = True
+        response = await async_client.delete(
+            f"/directories/translation/en/test/sutta/test/test1/", params={"dry_run": False}
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert "message" in response.json()
+        assert "main_task_id" in response.json()
+        assert "related_paths_task_id" in response.json()
+        assert response.json()["message"] == "Deletion successful"
+        assert response.json()["main_task_id"] == main_path_task_id
+        assert response.json()["related_paths_task_id"] == related_paths_task_id
