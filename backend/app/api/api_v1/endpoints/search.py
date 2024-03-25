39dee35917635c8fddd396340ef3696103d94f78
Marcin <17402897+mkorlaga@users.noreply.github.com>
2024-03-25 10:58:58 +0100

    Segment hints search (#39)

--- backend/app/api/api_v1/endpoints/search.py
+++ backend/app/api/api_v1/endpoints/search.py
@@ -2,7 +2,8 @@ from typing import Annotated
 
 from app.db.schemas.user import UserBase
 from app.services.auth.utils import get_current_user
-from app.services.search.models import SearchSegmentOut
+from app.services.search.models import SearchSegmentOut, TranslationHintsOut
+from app.services.users.permissions import is_user_active
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from search.search import Search
 
@@ -39,3 +40,20 @@ async def search(
         )
     params["uid"] = uid
     return SearchSegmentOut(results=es.get_segments(size, page, params))
+
+
+@router.get("/hints/", response_model=list[TranslationHintsOut], dependencies=[Depends(is_user_active)])
+async def get_translation_hints(
+    source_muid: str, target_muid: str, segment_id: str, text_value: str
+) -> list[TranslationHintsOut]:
+    similar_phrases: list[dict] = es.get_phrase_similar_segments(text_value, source_muid)
+    translation_hints = es.get_segment_value_for_uids_and_muid(
+        [phrase_dict.get("uid") for phrase_dict in similar_phrases], target_muid
+    )
+    similar_phrases_with_translation = es.merge_segments_with_translation_hints(similar_phrases, translation_hints)
+    translation_hints = es.aggregate_similar_segments(similar_phrases_with_translation)
+    return [
+        TranslationHintsOut(**hint)
+        for hint in translation_hints
+        if hint.get("uid") != segment_id and hint.get("translation_hints")
+    ]
